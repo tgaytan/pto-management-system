5e2340ab0be8862c29e83b8bcfb4b4d52c7a0dbc
tgaytan <tgaytan14@utexas.edu>
2024-05-05 20:50:00 -0500

    updated model so password is encrypted before it's saved. the login endpoint now compares the password entered to the password in the database

--- server/controllers/api/index.js
+++ server/controllers/api/index.js
@@ -1,8 +1,14 @@
 const router = require('express').Router()
+const { Employee } = require('../../models')
 
-router.post('/login', (req, res) => {
+router.post('/login', async (req, res) => {
     try {
         console.log('api/login route hit!')
+        const employee = await Employee.findOne({ email: req.body.email })
+
+        const correctPw = await employee.isCorrectPassword(req.body.password)
+        console.log(correctPw)
+
         res.status(200).json({ message: 'route hit successfully' })
     } catch (err) {
         console.log(err)
--- server/models/Employee.js
+++ server/models/Employee.js
@@ -1,4 +1,5 @@
 const { Schema, model } = require('mongoose')
+const bcrypt = require('bcrypt')
 
 const employeeSchema = new Schema({
     email: {
@@ -14,6 +15,21 @@ const employeeSchema = new Schema({
         trim: true
     }
 })
+
+// set up pre-save middleware to create password
+employeeSchema.pre('save', async function (next) {
+	if (this.isNew || this.isModified('password')) {
+		const saltRounds = 10
+		this.password = await bcrypt.hash(this.password, saltRounds)
+	}
+
+	next()
+})
+
+// compare the incoming password with the hashed password
+employeeSchema.methods.isCorrectPassword = async function (password) {
+	return await bcrypt.compare(password, this.password)
+}
 
 const Employee = model('Employee', employeeSchema)
 
