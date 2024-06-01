c3847a2fff39409c4554e97d7d86673c6bcfdaa7
tgaytan <tgaytan14@utexas.edu>
2024-05-31 22:05:41 -0500

    used tailwind components to list users who will be out on a certain day.  right now it just has a static list from tailwind

--- client/src/pages/RequestTimeOff.jsx
+++ client/src/pages/RequestTimeOff.jsx
@@ -13,8 +13,63 @@ function RequestTimeOff() {
 
         const response = await fetch(`http://localhost:3001/api/getDaysOff/${numFormat}`)
         const data = await response.json()
-        console.log(data)
-    } 
+        console.log(data.daysOff.length)
+    }
+
+    const people = [
+            {
+            name: 'Leslie Alexander',
+            email: 'leslie.alexander@example.com',
+            role: 'Co-Founder / CEO',
+            imageUrl:
+                'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+            lastSeen: '3h ago',
+            lastSeenDateTime: '2023-01-23T13:23Z',
+            },
+            {
+            name: 'Michael Foster',
+            email: 'michael.foster@example.com',
+            role: 'Co-Founder / CTO',
+            imageUrl:
+                'https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+            lastSeen: '3h ago',
+            lastSeenDateTime: '2023-01-23T13:23Z',
+            },
+            {
+            name: 'Dries Vincent',
+            email: 'dries.vincent@example.com',
+            role: 'Business Relations',
+            imageUrl:
+                'https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+            lastSeen: null,
+            },
+            {
+            name: 'Lindsay Walton',
+            email: 'lindsay.walton@example.com',
+            role: 'Front-end Developer',
+            imageUrl:
+                'https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+            lastSeen: '3h ago',
+            lastSeenDateTime: '2023-01-23T13:23Z',
+            },
+            {
+            name: 'Courtney Henry',
+            email: 'courtney.henry@example.com',
+            role: 'Designer',
+            imageUrl:
+                'https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+            lastSeen: '3h ago',
+            lastSeenDateTime: '2023-01-23T13:23Z',
+            },
+            {
+            name: 'Tom Cook',
+            email: 'tom.cook@example.com',
+            role: 'Director of Product',
+            imageUrl:
+                'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+            lastSeen: null,
+        },
+    ]
 
     const { firstName, lastName, remainingPTO } = Auth.getProfile().data
 
@@ -22,13 +77,13 @@ function RequestTimeOff() {
         <div>
             <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
                 <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-                    <label htmlFor="employee" className="block text-sm font-medium leading-6 text-gray-900">
-                        Name
-                    </label>
                     <div className="relative mt-2 rounded-md shadow-sm">
-                        <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
-                        {/* <span className="text-gray-500 sm:text-sm">$</span> */}
-                        </div>
+                        {/* <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
+                        <span className="text-gray-500 sm:text-sm">$</span>
+                        </div> */}
+                        <label htmlFor="employee" className="block text-sm font-medium leading-6 text-gray-900">
+                        Name
+                        </label>
                         <input
                         type="text"
                         name="employee-name"
@@ -66,8 +121,38 @@ function RequestTimeOff() {
                         defaultValue={remainingPTO}
                         />
                     </div>
-                    <div className="absolute left-1/2 z-10 mt-5 flex w-screen max-w-max -translate-x-1/2 px-4">
+                    <div className="relative mt-2 rounded-md shadow-sm">
                         <Calendar maxDate={maxDate} minDate={nextDay} onClickDay={onClickDay} />
+                    </div>
+                    <div className="relative mt-2 rounded-md shadow-sm">
+                        <ul role="list" className=" divide-gray-100">
+                            {people.map((person) => (
+                                <li key={person.email} className="flex justify-between gap-x-6 py-2">
+                                <div className="flex min-w-0 gap-x-4">
+                                    {/* <img className="h-12 w-12 flex-none rounded-full bg-gray-50" src={person.imageUrl} alt="" /> */}
+                                    <div className="min-w-0 flex-auto">
+                                        <p className="text-sm font-semibold leading-6 text-gray-900">{person.name}</p>
+                                        {/* <p className="mt-1 truncate text-xs leading-5 text-gray-500">{person.email}</p> */}
+                                    </div>
+                                </div>
+                                <div className="hidden shrink-0 sm:flex sm:flex-col sm:items-end">
+                                    <p className="text-sm leading-6 text-gray-900">{person.role}</p>
+                                    {/* {person.lastSeen ? (
+                                    <p className="mt-1 text-xs leading-5 text-gray-500">
+                                        Last seen <time dateTime={person.lastSeenDateTime}>{person.lastSeen}</time>
+                                    </p>
+                                    ) : (
+                                    <div className="mt-1 flex items-center gap-x-1.5">
+                                        <div className="flex-none rounded-full bg-emerald-500/20 p-1">
+                                        <div className="h-1.5 w-1.5 rounded-full bg-emerald-500" />
+                                        </div>
+                                        <p className="text-xs leading-5 text-gray-500">Online</p>
+                                    </div>
+                                    )} */}
+                                </div>
+                                </li>
+                            ))}
+                        </ul>
                     </div>
                 </div>
             </div>
