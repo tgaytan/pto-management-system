87d6780d2704b1402765e590f9f825e25ae53591
tgaytan <tgaytan14@utexas.edu>
2024-06-01 13:24:05 -0500

    when clicking on a day, it now shows who is out for that day

--- client/src/pages/RequestTimeOff.jsx
+++ client/src/pages/RequestTimeOff.jsx
@@ -21,7 +21,6 @@ function RequestTimeOff() {
         setDayClicked(true)
         setPeopleOff(data.daysOff)
         console.log(data.daysOff)
-        console.log(peopleOff)
     }
 
     const people = [
@@ -78,6 +77,9 @@ function RequestTimeOff() {
             lastSeen: null,
         },
     ]
+
+    console.log(peopleOff)
+    console.log(dayClicked)
 
     const { firstName, lastName, remainingPTO } = Auth.getProfile().data
 
@@ -141,28 +143,14 @@ function RequestTimeOff() {
                         }
                         <ul role="list" className="divide-gray-100">
                             {peopleOff.map((person) => (
-                                <li key={person.emloyeeId.email} className="flex justify-between gap-x-6 py-2">
+                                <li key={person.employeeId.email} className="flex justify-between gap-x-6 py-2">
                                 <div className="flex min-w-0 gap-x-4">
-                                    {/* <img className="h-12 w-12 flex-none rounded-full bg-gray-50" src={person.imageUrl} alt="" /> */}
                                     <div className="min-w-0 flex-auto">
-                                        <p className="text-sm font-semibold leading-6 text-gray-900">{person.employeeId.firstName + person.employeeId.lastName }</p>
-                                        {/* <p className="mt-1 truncate text-xs leading-5 text-gray-500">{person.email}</p> */}
+                                        <p className="text-sm font-semibold leading-6 text-gray-900">{`${person.employeeId.firstName} ${person.employeeId.lastName}`}</p>
                                     </div>
                                 </div>
                                 <div className="hidden shrink-0 sm:flex sm:flex-col sm:items-end">
-                                    <p className="text-sm leading-6 text-gray-900">{person.dayOff}</p>
-                                    {/* {person.lastSeen ? (
-                                    <p className="mt-1 text-xs leading-5 text-gray-500">
-                                        Last seen <time dateTime={person.lastSeenDateTime}>{person.lastSeen}</time>
-                                    </p>
-                                    ) : (
-                                    <div className="mt-1 flex items-center gap-x-1.5">
-                                        <div className="flex-none rounded-full bg-emerald-500/20 p-1">
-                                        <div className="h-1.5 w-1.5 rounded-full bg-emerald-500" />
-                                        </div>
-                                        <p className="text-xs leading-5 text-gray-500">Online</p>
-                                    </div>
-                                    )} */}
+                                    <p className="text-sm leading-6 text-gray-900">{new Date(person.dayOff).toISOString().split('T')[0]}</p>
                                 </div>
                                 </li>
                             ))}
