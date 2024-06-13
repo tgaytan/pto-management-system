fd681fa460ffb16ec87d2ad470124d5364ff2c03
tgaytan <tgaytan14@utexas.edu>
2024-06-12 22:04:38 -0500

    removed profile pic and notifications icon from nav bar. they didn't do anything. they were just there as part of default tailwind component

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -62,7 +62,7 @@ export default function Header() {
                     </div>
                     </div>
                 </div>
-                <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+                {/* <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
                     <button
                     type="button"
                     className="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
@@ -72,7 +72,7 @@ export default function Header() {
                     <BellIcon className="h-6 w-6" aria-hidden="true" />
                     </button>
 
-                    {/* Profile dropdown */}
+                    Profile dropdown // this use to be a comment by itself
                     <Menu as="div" className="relative ml-3">
                     <div>
                         <Menu.Button className="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
@@ -128,7 +128,7 @@ export default function Header() {
                         </Menu.Items>
                     </Transition>
                     </Menu>
-                </div>
+                </div> */}
                 </div>
             </div>
 
