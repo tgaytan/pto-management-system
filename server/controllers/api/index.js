d87eb88a0c956d644b96efb6c5bd8be6735871f5
tgaytan <tgaytan14@utexas.edu>
2024-06-12 21:49:07 -0500

    fixed another bug that caused server to crash. if request with 0 PTO hours was sent, node would have two lines of res.send that would run and cause it to crash

--- server/controllers/api/index.js
+++ server/controllers/api/index.js
@@ -40,12 +40,13 @@ router.get('/getAllDaysOff/:employeeId', async (req, res) => {
 router.post('/addDayOff', async (req, res) => {
     try {
         // getting amount of PTO left over
-        const { remainingPTO } = await Employee.findOne({ _id: req.body.employeeId })
-        let employee // defining this variable so I can assign the new updated employee document to it. then use it to sign a new jwt token
+        let employee = await Employee.findOne({ _id: req.body.employeeId })
+        const { remainingPTO } = employee // defining this variable so I can assign the new updated employee document to it. then use it to sign a new jwt token
 
         // checks if the user has enough PTO hours to request a day off. if they do, it subtracts it from their current hours and add the day off
         if (remainingPTO < req.body.hours) {
             res.status(403).json({ message: 'Not enough hours remaining', token: req.body.token })
+            return
         } else {
             employee = await Employee.findOneAndUpdate(
                 { _id: req.body.employeeId }, 
