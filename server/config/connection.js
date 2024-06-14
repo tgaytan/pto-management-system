296533a3a6d87ada78417843d36fd14809164f51
tgaytan <tgaytan14@utexas.edu>
2024-06-13 22:38:23 -0500

    still trying to work out Render errors. however data is now seeded in the cloud mongoDB database

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,8 @@
 const mongoose = require('mongoose')
+require('dotenv').config()
 
-mongoose.connect('mongodb://127.0.0.1:27017/ptoDB')
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/ptoDB')
+
+console.log(process.env.MONGODB_URI)
 
 module.exports = mongoose.connection
